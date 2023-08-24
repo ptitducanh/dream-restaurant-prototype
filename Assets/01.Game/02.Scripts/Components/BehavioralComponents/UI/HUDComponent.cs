23f8cb5f321a073b661f529d15fd724428c694a4
Le Duc Anh <ptitducanh@gmail.com>
2023-08-24 09:11:14 +0700

    UI and text

---
+++ Assets/01.Game/02.Scripts/Components/BehavioralComponents/UI/HUDComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Scripts.Entities;
+using TMPro;
+using UnityEngine;
+
+namespace Scripts.Components.BehavioralComponents
+{
+    public class HUDComponent : BehavioralComponent
+    {
+        [SerializeField] private TMP_Text[] coinTexts;
+        
+        private InventoryDataComponent _inventoryDataComponent;
+        private int _currentCoinAmount;
+        
+        public override void OnAwake()
+        {
+            base.OnAwake();
+
+            var characterEntity = EntityManager.Instance.GetEntitiesByType<PlayerCharacterEntity>().FirstOrDefault();
+            if (characterEntity == null) return;
+            
+            _inventoryDataComponent = characterEntity.GetDataComponent<InventoryDataComponent>();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            
+            if (_inventoryDataComponent.GetIntStat("Coin") == _currentCoinAmount) return;
+            _currentCoinAmount = _inventoryDataComponent.GetIntStat("Coin");
+            var coinAmountText = _currentCoinAmount.ToString();
+            foreach (var text in coinTexts)
+            {
+                text.text = coinAmountText;
+            }
+        }
+    }
+}
